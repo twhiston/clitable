d98fcd8dc2a552b07a2fe37f206a96997fe04826
Tom Whiston <tom.whiston@gmail.com>
2017-11-11 02:19:15 +0100

    working column padding for rows of differing lengths

--- table.go
+++ table.go
@@ -11,16 +11,20 @@ type row []string
 
 // Table represents the table to be printed
 type Table struct {
-	cs   colSize
-	rows []row
-	Fmt  TableFormat
+	columnSizes    colSize
+	maximumColumns int
+	rows           []row
+	Fmt            TableFormat
+	Writer         io.Writer
+	ErrWriter      io.Writer
 }
 
 type TableFormat struct {
 	Corner string
 	Row    string
 	Column string
-	Value  string
+	Pad    string
+	Blank  string
 }
 
 func (c colSize) setIfBigger(k, v int) {
@@ -33,33 +37,56 @@ func (c colSize) setIfBigger(k, v int) {
 // New creates a new table
 func New() *Table {
 	t := new(Table)
-	t.cs = make(colSize)
+	t.columnSizes = make(colSize)
 	t.Fmt.Corner = "+"
 	t.Fmt.Row = "-"
-	t.Fmt.Column = "|"
-	t.Fmt.Value = " "
+	t.Fmt.Column = "| "
+	t.Fmt.Pad = " "
+	t.Fmt.Blank = " "
+	t.Writer = os.Stdout
+	t.ErrWriter = os.Stderr
 	return t
 }
 
 // AddRow adds a row to the table
 func (t *Table) AddRow(cols ...string) *Table {
+	colCount := len(cols)
 	var r row
 	for i, col := range cols {
-		t.cs.setIfBigger(i, len(col))
+		t.columnSizes.setIfBigger(i, len(col))
 		r = append(r, col)
+
 	}
 	t.rows = append(t.rows, r)
+
+	if colCount < t.maximumColumns {
+		t.ColumnPadding()
+	} else if colCount > t.maximumColumns {
+		t.maximumColumns = colCount
+		t.ColumnPadding()
+	}
 	return t
+}
+
+func (t *Table) ColumnPadding() {
+	for k, v := range t.rows {
+		colCount := len(v)
+		for colCount < t.maximumColumns {
+			v = append(v, t.Fmt.Blank)
+			colCount += 1
+		}
+		t.rows[k] = v
+	}
 }
 
 // Print prints the table to standard output
 func (t *Table) Print() {
-	t.Fprint(os.Stdout)
+	t.Fprint(t.Writer)
 }
 
 // Errprint prints the table to error output
 func (t *Table) Errprint() {
-	t.Fprint(os.Stderr)
+	t.Fprint(t.ErrWriter)
 }
 
 // Fprint prints the table to any io.Writer of your choice
@@ -72,16 +99,18 @@ func (t *Table) Fprint(w io.Writer) {
 }
 
 func (t *Table) printRow(row row, w io.Writer) {
+	colCount := 0
 	for k, col := range row {
 		c := string(col)
-		fmt.Fprint(w, t.Fmt.Column, pad(c+"", t.cs[k]+1, t.Fmt.Value))
+		fmt.Fprint(w, t.Fmt.Column, pad(c+"", t.columnSizes[k]+1, t.Fmt.Pad))
+		colCount += 1
 	}
 	fmt.Fprintf(w, t.Fmt.Column+"\n")
 }
 
 func (t *Table) printEmpty(w io.Writer) {
-	for i := 0; i < len(t.cs); i ++ {
-		fmt.Fprint(w, t.Fmt.Corner+pad("", t.cs[i]+1, t.Fmt.Row))
+	for i := 0; i < len(t.columnSizes); i ++ {
+		fmt.Fprint(w, t.Fmt.Corner+pad("", t.columnSizes[i]+len(t.Fmt.Column), t.Fmt.Row))
 	}
 	fmt.Fprintf(w, t.Fmt.Corner+"\n")
 }
--- table_test.go
+++ table_test.go
@@ -2,26 +2,50 @@ package clitable
 
 import (
 	"testing"
+	"bytes"
 )
 
 func TestTable(t *testing.T) {
 	table := New()
-	if len(table.cs) != 0 {
+	if len(table.columnSizes) != 0 {
 		t.Error("initialization error")
 	}
-	
+
 	table.AddRow("One", "Two", "Three")
-	if table.cs[0] != 3 || table.cs[1] != 3 || table.cs[2] != 5 {
+	if table.columnSizes[0] != 3 || table.columnSizes[1] != 3 || table.columnSizes[2] != 5 {
 		t.Error("Counting error")
 	}
 
 	table.AddRow("One", "Two", "MoreThanThree")
-	if table.cs[0] != 3 || table.cs[1] != 3 || table.cs[2] != 13 {
+	if table.columnSizes[0] != 3 || table.columnSizes[1] != 3 || table.columnSizes[2] != 13 {
 		t.Error("re-Counting error")
 	}
 
 	table.AddRow("a", "a", "a")
-	if table.cs[0] != 3 || table.cs[1] != 3 || table.cs[2] != 13 {
+	if table.columnSizes[0] != 3 || table.columnSizes[1] != 3 || table.columnSizes[2] != 13 {
 		t.Error("re-Counting error")
+	}
+}
+
+func TestTableOther(t *testing.T) {
+
+	table := New()
+	//table.Fmt.Corner = "DICK"
+	table.AddRow("One", "Two", "Three")
+	table.AddRow("xxxxxxx", "rr")
+	table.AddRow("a", "b", "c", "d", "e")
+
+	expectedOutput := `+---------+-----+-------------------+
+| One     | Two | Three             |
++---------+-----+-------------------+
+| xxxxxxx | rr  | Thrffffffffffffee |
++---------+-----+-------------------+
+`
+
+	buffer := new(bytes.Buffer)
+	table.Writer = buffer
+	table.Print()
+	if buffer.String() != expectedOutput {
+		t.Fatal("incorrect output.\nExpected:\n", expectedOutput, "\nActual:\n", buffer.String())
 	}
 }
